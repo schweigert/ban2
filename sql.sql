52063832f95f75e70c21491f55389b991ab97b38
fleyhe <fleyhe0@gmail.com>
2017-08-30 20:38:14 -0300

    Atividades

--- sql.sql
+++
@@ -1,7 +0,0 @@
-SELECT nome FROM Funcionarios
-SUBTRACT
-SELECT f1.nome FROM Funcionarios f1 JOIN Funcionarios f2 ON f1.codf <> f2.codf AND f1.idade < f2.idade
-
--- Minha ideia
-
-SELECT f.nome FROM Funcionarios f WHERE NOT EXIST (SELECT * FROM Funcionarios f1 WHERE f1.idade > f.idade)
